b1cf8008f5416241f8031f978b5be34af08b7c09
DCsunset <DCsunset@users.noreply.github.com>
2022-02-18 14:11:31 -0500

    chore(webui): add proxy for development

--- webui/vite.config.ts
+++ webui/vite.config.ts
@@ -4,7 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    port: 8080
+    port: 8080,
+    proxy: {
+      '/socket.io': {
+        target: "ws://localhost:3000",
+        changeOrigin: true,
+        ws: true
+      }
+    }
   },
   plugins: [react()]
 })
